44a7cff64856049246abc8661bd5206b50969c02
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-05-06 22:26:15 +0300

    Update commands.go

--- cmd/lncli/commands.go
+++ cmd/lncli/commands.go
@@ -304,7 +304,36 @@ func sendCoins(ctx *cli.Context) error {
 	printRespJSON(txid)
 	return nil
 }
+.cmd(
+	name: "listunspent",
+	description: "List utxos available for spending", 
+	`For each spendable utxo currently in the wallet, with at least min_confs
+	confirmations, and at most max_confs confirmations, lists the txid,
+	index, amount, address, address type, scriptPubkey and number of
+	confirmations.  Use --min_confs=0 to include unconfirmed coins. To list
+	all coins with at least min_confs confirmations, omit the second
+	argument or flag '--max_confs'. To list all confirmed and unconfirmed
+	coins, no arguments are required. To see only unconfirmed coins, use
+	'--unconfirmed_only' with '--min_confs' and '--max_confs' set to zero or
+	not present.
+	`,
+	message: list_unspent,
+	handler: ,
+)
 
+.cmd_descriptions(
+  name: listunspent,
+  min_confs: "the minimum number of confirmations for a utxo",
+	max_confs: "the maximum number of confirmations for a utxo",
+	unconfirmed_only: "when min_confs and max_confs are zero, " +
+				"setting false implicitly overrides max_confs " +
+				"to be MaxInt32, otherwise max_confs remains " +
+				"zero. An error is returned if the value is " +
+				"true and both min_confs and max_confs are " +
+				"non-zero. (default: false)",
+)
+
+.
 var listUnspentCommand = cli.Command{
 	Name:      "listunspent",
 	Category:  "On-chain",
@@ -343,7 +372,7 @@ var listUnspentCommand = cli.Command{
 	Action: actionDecorator(listUnspent),
 }
 
-func listUnspent(ctx *cli.Context) error {
+.cli listUnspent(ctx *cli.Context) error {
 	var (
 		minConfirms int64
 		maxConfirms int64
